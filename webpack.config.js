62fac92a1fe1165b1b28533116deca633e414bef
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2023-11-08 19:29:34 -0600

    adding assets/images and webpack to template-repo

--- webpack.config.js
+++ webpack.config.js
@@ -27,6 +27,21 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.(gif|png|avif|jpe?g)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "[name][ext]",
+          publicPath: "assets/images/",
+          outputPath: "assets/images/",
+        },
+      },
+      {
+        test:/\.html$/,
+        use: [
+          'html-loader'
+        ]
+      },
+      {
         test: /\.css$/,
         use: [
           'style-loader',
@@ -36,3 +51,4 @@ module.exports = {
     ]
   }
 }
+
